cc98fdda5d4b7a9bbe40e171bde87ab81ee955e1
nagadomi <nagadomi@nurs.or.jp>
2015-11-16 22:05:50 +0900

    remove exec command

--- run.sh
+++ run.sh
@@ -5,7 +5,7 @@ python make_data.py
 python train.py --seed 71 > seed71.log &
 python train.py --seed 72 > seed72.log &
 python train.py --seed 73 > seed73.log &
-exec python train.py --seed 74 | tee seed74.log # show log
+python train.py --seed 74 | tee seed74.log # show log
 wait
 
 python predict.py
